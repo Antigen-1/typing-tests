edad4532b13ca32603b11a9f3394956651ffcc9b
Antigen-1 <zhanghao@antigen.top>
2023-08-22 16:01:25 +0800

    format the codes

--- main.rkt
+++ main.rkt
@@ -27,29 +27,24 @@
 
 (require racket/string racket/generator racket/runtime-path)
 
-(define-runtime-path database "./sample_paragraphs.txt")
+(define-runtime-path default "./sample_paragraphs.txt")
 
-;;topics
-(define punctuation #px"\\p{P}")
-
+;;punctuation
+(define punctuation #px"\\p{P}+")
 (define (remove-punctuation str)
-  ;;remove puctuation characters from STR.
+  ;;remove punctuation characters from STR.
   (string-replace str punctuation ""))
 
-(define (topic? str)
-  ;;ensure that STR is a topic.
-  (and (string=? str (string-downcase str)) (not (regexp-match punctuation str))))
-
-(module+ test
-  (check-true (topic? "cat"))
-  (check-false (or (topic? "Cat") (topic? "cat.")))
-  
+(module+ test  
   (check-true (string=? "cat" (remove-punctuation "cat.")))
   (check-true (string=? "Its" (remove-punctuation "It's"))))
 
-;;paragraphs
+;;paragraphs and topics
+(define (topic? str)
+  ;;ensure that STR is a topic.
+  (and (string=? str (string-downcase str)) (not (regexp-match punctuation str))))
 (define (with-topics? para tops)
-  ;;check whether PARA contains elements in TOPS.
+  ;;check whether PARA contains one of the topics in TOPS.
   (let/cc break
     (for ((seg (in-list (string-split para))))
       (for ((top (in-list tops)))
@@ -65,6 +60,9 @@
    #f))
 
 (module+ test
+  (check-true (topic? "cat"))
+  (check-false (or (topic? "Cat") (topic? "cat.")))
+  
   (define gen (make-paragraph-generator (in-list (list "a" "ab" "abcde")) (lambda (s) (<= (string-length s) 3))))
   (check-true (string=? (gen) "a"))
   (check-true (string=? (gen) "ab"))
@@ -97,48 +95,50 @@
   (check-true (= 0.0 (accuracy "a" "")))
   (check-true (= 100.0 (accuracy "" ""))))
 
-;;IO
+;;console IO
 (define (universal-read-line)
   ;;just an alias
   (read-line (current-input-port) 'any))
 (define (format-and-displayln template . segments)
+  ;;print formatted strings
   (displayln (apply format template segments)))
 (define (input-speed)
   ;;return the typed string and the words-per-minute (WPM) of the typed string.
   (call-with-values (lambda () (time-apply universal-read-line null))
                     (lambda (lst cpu real gc)
                       (define typed (car lst))
-                      (values typed
-                              (/ (/ (string-length typed) 5.0) (/ real 60000.0))))))
-(define (step func)
-  ;;run the given FUNC and ask whether we are going to proceed.
-  (func)
-  (newline)
-  (displayln "Press enter/return for the next paragraph or type q to quit.")
-  (not (string=? "q" (universal-read-line))))
-(define (make-paragraph-logging-handler topics gen)
+                      (values typed (/ (/ (string-length typed) 5.0) (/ real 60000.0))))))
+(define (make-paragraph-logging-handler topics)
   ;;when one argument is provided, it creates a procedure that prints the maximum WPM and accuracy.
   ;;when no argument is provided, it creates a procedure in terms of GEN that asks for input, and then computes and print the accuracy and WPM.
-  (define max-wpm (box 0.0))
+  (define max-WPM (box 0.0))
   (define max-accuracy (box 0.0))
   (case-lambda
-    ((_) (format-and-displayln "maximum wpm: ~a" (unbox max-wpm))
-         (format-and-displayln "maximum accuracy: ~a" (unbox max-accuracy)))
-    (()
-     (define reference (gen))
+    (() (format-and-displayln "maximum WPM: ~a" (unbox max-WPM))
+        (format-and-displayln "maximum accuracy: ~a" (unbox max-accuracy)))
+    ((reference)
      (cond (reference
             (displayln "Type the following paragraph and then press enter/return.")
             (displayln "If you only type part of it, you will be scored only on that part.")
             (newline)
             (displayln reference)
             (newline)
-            (define-values (typed wpm) (input-speed))
+            (define-values (typed WPM) (input-speed))
             (define accur (accuracy typed reference))
             (cond ((> accur (unbox max-accuracy)) (set-box! max-accuracy accur)))
-            (cond ((> wpm (unbox max-wpm)) (set-box! max-wpm wpm)))
-            (format-and-displayln "wpm: ~a" wpm)
+            (cond ((> WPM (unbox max-WPM)) (set-box! max-WPM WPM)))
+            (format-and-displayln "WPM: ~a" WPM)
             (format-and-displayln "accuracy: ~a" accur))
            (else (format-and-displayln "No more paragraphs about ~s are available." topics))))))
+(define (make-main-loop output gen)
+  ;;run the given OUTPUT and ask whether we are going to proceed.
+  (let loop ()
+    (output (gen))
+    (newline)
+    (displayln "Press enter/return for the next paragraph or type q to quit.")
+    (if (string=? "q" (universal-read-line))
+        (output)
+        (loop))))
 
 (module+ main
   ;; (Optional) main submodule. Put code here if you need it to be executed when
@@ -147,7 +147,7 @@
   ;; http://docs.racket-lang.org/guide/Module_Syntax.html#%28part._main-and-test%29
 
   (require racket/cmdline raco/command-name racket/contract)
-  (define database-box (box database))
+  (define database-box (box default))
   (command-line
     #:program (short-program+command-name)
     #:once-any
@@ -164,8 +164,6 @@
          (unbox database-box)
          (lambda (in)
            (define gen (make-paragraph-generator (in-lines in) (lambda (l) (with-topics? l lst))))
-           (define handler (make-paragraph-logging-handler lst gen))
-           (let loop ()
-             (cond ((step handler) (loop))
-                   (else (handler 'result)))))))
+           (define handler (make-paragraph-logging-handler lst))
+           (make-main-loop handler gen))))
      '("Specify the topics")]))
